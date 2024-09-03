e4c696c04c0770db24369147fd6b9e64504d7df4
Eysteinn Örn Jónsson <eysi@live.com>
2024-09-03 12:15:11 +0000

    updated scene

--- scripts/utils/mainManager.gd
+++ scripts/utils/mainManager.gd
@@ -15,12 +15,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -42,8 +45,13 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func _on_music_finished():
+	pass # Replace with function body.
